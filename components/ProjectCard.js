7a64201b89d57b7767f502a63b5865be5ab6c42d
Aakash-Sondagar <aakashsondar@gmail.com>
2024-12-19 22:59:20 +0530

    Fixes

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -8,14 +8,14 @@ const ProjectCard = (list) => {
           key={index}
           className="group relative bg-[#F9F2FF] border-r-2 border-b-2 border-[#E4C8Ff] hover:border-r-0"
         >
-          <div className="relative z-10 items-center justify-center overflow-hidden transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3">
+          <div className="relative z-10 items-center justify-center overflow-hidden transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-2 p-2 hover:p-0 hover:pl-3">
             <div onClick={() => window.open(item.link, "_blank")} className="">
               <Image
                 src={item.image}
                 alt={item.title}
                 width={500}
                 height={500}
-                className="w-full h-full object-cover p-2"
+                className="w-full h-full object-cover"
               />
             </div>
             <div className="px-4 py-4">
@@ -27,7 +27,7 @@ const ProjectCard = (list) => {
               </div>
             </div>
           </div>
-          <div class="absolute inset-0 z-0 h-full w-full transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a67cce,10px_10px_#ccb4e3,15px_15px_#e5d9f1]"></div>
+          <div class="absolute inset-0 z-0 h-full w-full transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-2 group-hover:[box-shadow:10px_10px_#a67cce]"></div>
         </div>
       ))}
     </>
