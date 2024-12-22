5e98dbf2c65cd25561dbdff870a1ba8b4800f4b4
Aakash-Sondagar <aakashsondar@gmail.com>
2024-12-22 18:08:04 +0530

    Project page added

---
+++ app/projects/page.js
@@ -0,0 +1,55 @@
+"use client";
+import { Esteban } from "next/font/google";
+import { useState } from "react";
+
+import { Projects } from "@/lib/staticContent";
+import ProjectCard from "@/components/ProjectCard";
+
+const esteban = Esteban({ subsets: ["latin"], weight: "400" });
+
+const uniqueTags = [
+  "all",
+  ...new Set(Projects.flatMap((project) => project.tags)),
+];
+
+export default function ProjectPage() {
+  const [activeFilter, setActiveFilter] = useState("all");
+
+  const filteredProjects =
+    activeFilter === "all"
+      ? Projects
+      : Projects.filter((project) => project.tags.includes(activeFilter));
+
+  return (
+    <div
+      style={{ fontFamily: "Karla, sans-serif" }}
+      className="px-6 md:px-16 lg:px-64"
+    >
+      <h2 className={`${esteban.className} text-4xl font-normal pb-6`}>
+        professional experience
+      </h2>
+      <div className=" font-medium text-lg mb-6">sort by area</div>
+      <div className="filters flex flex-wrap gap-2 mb-4 lg:mb-12">
+        {uniqueTags.map((tag) => (
+          <button
+            key={tag}
+            className={`relative group bg-[#F9F2FF] border-r-2 border-b-2 border-[#E4C8FF] hover:border-r-0 hover:border-b-0 transition-all duration-300 px-4 py-2 text-base ${
+              activeFilter === tag
+                ? "bg-[#671CAD] text-white"
+                : "text-[#671CAD]"
+            }`}
+            onClick={() => setActiveFilter(tag)}
+          >
+            <span>{tag}</span>
+
+            {/* Hover shadow effect */}
+            <div className="absolute inset-0 z-0 h-full w-full bg-transparent transition-all duration-300 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:shadow-[4px_4px_#a67cce]"></div>
+          </button>
+        ))}
+      </div>
+      <div className="project-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {ProjectCard(filteredProjects)}
+      </div>
+    </div>
+  );
+}
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -6,10 +6,10 @@ const ProjectCard = (list) => {
       {list.map((item, index) => (
         <div
           key={index}
-          className="group relative bg-[#F9F2FF] border-r-2 border-b-2 border-[#E4C8FF] hover:border-r-0 hover:border-b-0 transition-all duration-300"
+          className="group relative bg-[#F9F2FF] cursor-pointer border-r-2 border-b-2 border-[#E4C8FF] hover:border-r-0 hover:border-b-0 transition-all duration-300"
         >
           {/* Inner content container */}
-          <div className="relative z-10 items-center justify-center overflow-hidden transition-transform duration-300 group-hover:-translate-x-3 group-hover:-translate-y-2 p-2">
+          <div className="relative z-10 items-center justify-center overflow-hidden transition-transform duration-300 p-2">
             <div onClick={() => window.open(item.link, "_blank")} className="">
               <Image
                 src={item.image}
@@ -30,7 +30,7 @@ const ProjectCard = (list) => {
           </div>
 
           {/* Hover shadow effect */}
-          <div className="absolute inset-0 z-0 h-full w-full bg-transparent transition-all duration-300 group-hover:translate-x-1 group-hover:translate-y-1 group-hover:shadow-[10px_10px_#a67cce]"></div>
+          <div className="absolute inset-0 z-0 h-full w-full bg-transparent transition-all duration-300 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:shadow-[10px_10px_#a67cce]"></div>
         </div>
       ))}
     </>
